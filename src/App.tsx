2634b0a43c96cfcaae5dff0de88ab8ad2e0da648
mjrgeorge <mjrgeorge@gmail.com>
2022-11-25 21:11:01 +0600

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import './App.css';
 import MyModal from './components/MyModal';
+import ReactPdfView from './components/ReactPdfView';
 
 function App() {
   const fileUrl = "https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf"
+  // const fileUrl = "/src/components/documents/exampleWithAudioVideo.pdf"
 
   return (
     <div className="App">
       <h1>
         React PDF Viewer
       </h1>
-      {/* <ReactPdfView fileUrl={fileUrl}/> */}
       <MyModal fileUrl={fileUrl}/>
+      <br />
+      <br />
+      <ReactPdfView fileUrl={fileUrl}/>
     </div>
   );
 }
